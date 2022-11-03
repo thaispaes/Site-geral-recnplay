813c486684471d39a762fdb524fe710459488672
thaispaes <thaisreginapaesdossantos@gmail.com>
2022-11-03 14:43:41 -0300

    feat: some changes

---
+++ assets/js/pagination.js
@@ -0,0 +1,14 @@
+let slides = document.querySelectorAll('.horario-slide');
+let index = 0;
+
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
